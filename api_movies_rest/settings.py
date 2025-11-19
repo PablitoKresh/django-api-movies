e3f74b0af446437c9d853231ee8ab2e9a381dd15
Pablo Jimenez Bertolet <pablopan13@gmail.com>
2025-11-19 15:14:54 +0100

    fixed allowed host for new domain

--- api_movies_rest/settings.py
+++ api_movies_rest/settings.py
@@ -30,7 +30,8 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    'django-api-movies.onrender.com'
+    'django-api-movies.onrender.com',
+    "api.pablobertolet.dev",
 ]
 
 
